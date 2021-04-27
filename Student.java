3dc29c406222f429af5b13b8c5fe52834b7ec210
JRHolder6263 <75594311+JRHolder6263@users.noreply.github.com>
2021-04-27 03:23:13 -0500

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,69 @@
+package StudentDataBaseApp;
+import java.util.Scanner;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int gradeYear;
+    private String studentID;
+    private String courses = "";
+    private int tuitionBalance = 0;
+    private static int costOfCourse = 600;
+    private static int id = 1000;
+
+    public Student () {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Student first name: ");
+        this.firstName = in.nextLine();
+        System.out.print("Enter Student last name: ");
+        this.lastName = in.nextLine();
+        System.out.print("1 - Freshmen\n2 - Sophomore\n3 - Junior\n4 - Senior" +
+                "\nEnter Student class level: ");
+        this.gradeYear = in.nextInt();
+        setStudentID();
+    }
+    //Generate ID
+    private void setStudentID() {
+        //Grade level + ID
+        id++;
+        this.studentID =  gradeYear + "" + id;
+    }
+
+    //Enroll In courses
+    public void enroll() {
+        //Get inside a loop, user hits 0
+        do {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Enter course to enroll (Q to quit): ");
+            String course = in.nextLine();
+            if (!course.equals("Q")) {
+                courses = courses + "\n\t" + course;
+                tuitionBalance = tuitionBalance + costOfCourse;
+            }
+            else { break; }
+        } while (1 != 0);
+    }
+
+    //View Balance and pay tuition
+    public void viewBalance() {
+        System.out.println("Your balance is: $" + tuitionBalance);
+    }
+
+    public void payTuition() {
+        viewBalance();
+        System.out.print("Enter your payment: $");
+        Scanner in = new Scanner(System.in);
+        int payment = in.nextInt();
+        tuitionBalance = tuitionBalance - payment;
+        System.out.println("Thank you for your payment of $" + payment);
+        viewBalance();
+    }
+    //Show Status
+    public String toString() {
+        return "Name: " + firstName + " " + lastName +
+                "\nGrade Level: " + gradeYear +
+                "\nStudent ID: " + studentID +
+                "\nCourses Enrolled: " + courses +
+                "\nBalance: $" + tuitionBalance;
+    }
+}
